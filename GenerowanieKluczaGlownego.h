7989d49e29870ae2240e44e8f251fb7f57afc8e7
PiotrekMarciniak <148332754+PiotrekMarciniak@users.noreply.github.com>
2024-04-16 21:43:01 +0200

    Add files via upload
    Generowanie klucza głównego potrzebnego do zaczęcia rundy

---
+++ GenerowanieKluczaGlownego.h
@@ -0,0 +1,5 @@
+#include <string>
+
+using namespace std;
+
+string KluczGlownyGenerator(int rozmiarKlucza);//deklaracja funkcji generuj¹cej klucz
---
+++ main.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+#include "GenerowanieKluczaGlownego.h"
+
+using namespace std;
+
+int main()
+{
+    int rozmiarKlucza = 16;
+    string KluczSzesnastkowo = KluczGlownyGenerator(rozmiarKlucza);
+    cout << "Wygenerowany klucz w formacie szesnastkowym: " << KluczSzesnastkowo << endl;
+    return 0;
+}
