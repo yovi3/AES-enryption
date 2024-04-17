a46e777dd31b967c09c4cba78c3f82bf3efc4b1a
PiotrekMarciniak <148332754+PiotrekMarciniak@users.noreply.github.com>
2024-04-17 22:53:21 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,12 +1,20 @@
 ﻿#include <iostream>
 #include "GenerowanieKluczaGlownego.h"
+#include "OczytIPodzialNaBajty.h"
+#include <fstream>
 
 using namespace std;
 
 int main()
 {
+    ofstream MyFile("kluczGlowny.txt"); //utworznie pliku
     int rozmiarKlucza = 16;
     string KluczSzesnastkowo = KluczGlownyGenerator(rozmiarKlucza);
     cout << "Wygenerowany klucz w formacie szesnastkowym: " << KluczSzesnastkowo << endl;
+    
+    MyFile << KluczSzesnastkowo; // zapisanie wygenerowanego klucza do pliku (ndapisuje co jest w pliku)
+
+    vector<char> macierzBajtow = odczytajPlik("krokiotwarciaplikow.txt");
+    
     return 0;
 }
