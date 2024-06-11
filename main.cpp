4701d4e8338a029aa8fb985e68369c39e74a0f42
PiotrekMarciniak <148332754+PiotrekMarciniak@users.noreply.github.com>
2024-06-11 05:19:19 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,9 @@
-﻿#include "FileHandler.h"
+#include "FileHandler.h"
 #include "KeyGenerator.h"
 #include "AES.h"
 #include <iostream>
+#include <sstream>
+#include <iomanip>
 
 int main() {
     try {
@@ -19,6 +21,19 @@ int main() {
         std::vector<unsigned char> encryptedData = AES::applyAES(fileData, key);
         std::cout << "Encrypted Data (Hex): ";
         FileHandler::printHex(encryptedData);
+
+        // Konwersja zaszyfrowanych danych do formatu szesnastkowego
+        std::ostringstream hexStream;
+        for (const auto& byte : encryptedData) {
+            hexStream << std::hex << std::setw(2) << std::setfill('0') << static_cast<int>(byte);
+        }
+        std::string hexString = hexStream.str();
+
+        // Zapisz dane szesnastkowe jako plik binarny
+        FileHandler::hexStringToBinaryFile(hexString, "encrypted_hex.dat");
+
+        std::cout << "Encrypted data in hex format saved to encrypted_hex.dat" << std::endl;
+
     }
     catch (const std::exception& e) {
         std::cerr << e.what() << std::endl;
