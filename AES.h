2f896b9ed8c4de908d54c36670c52df004597fd8
PiotrekMarciniak <148332754+PiotrekMarciniak@users.noreply.github.com>
2024-06-11 05:19:02 +0200

    Update AES.h

--- AES.h
+++ AES.h
@@ -2,6 +2,7 @@
 #define AES_H
 
 #include <vector>
+#include <string>
 
 class AES {
 public:
@@ -9,13 +10,13 @@ public:
     static void subBytes(std::vector<unsigned char>& data);
     static void shiftRows(std::vector<unsigned char>& data);
     static void mixColumns(std::vector<unsigned char>& data);
+    static void printState(const std::vector<unsigned char>& state, const std::string& label);
     static void addRoundKey(std::vector<unsigned char>& data, const std::vector<unsigned char>& roundKey);
     static std::vector<std::vector<unsigned char>> keyExpansion(const std::vector<unsigned char>& key);
     static std::vector<unsigned char> applyAES(const std::vector<unsigned char>& data, const std::vector<unsigned char>& key);
-    static std::vector<unsigned char> applyAESCBC(const std::vector<unsigned char>& data, const std::vector<unsigned char>& key, const std::vector<unsigned char>& iv);
-    static std::vector<unsigned char> applyAESECB(const std::vector<unsigned char>& data, const std::vector<unsigned char>& key); // Nowa funkcja
-    static std::vector<unsigned char> pkcs5Padding(const std::vector<unsigned char>& data);
     static const unsigned char sBox[256];
+private:
+    static uint8_t galois_multiplication(uint8_t a, uint8_t b);
 };
 
 #endif // AES_H
